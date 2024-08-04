e47f0e5071ef2de39034b6c3930b8e9b3ac47f00
dapk <pio4000@googlemail.com>
2024-08-04 02:24:03 +0200

    feat: poetry install and lock

--- app/ppcheck.py
+++ app/ppcheck.py
@@ -5,6 +5,7 @@ import inquirer
 import pyperclip
 import tomli
 from terminaltables import AsciiTable
+from inquirer.themes import GreenPassion, BlueComposure
 
 """
 author:     dapk@gmx.net
@@ -32,7 +33,13 @@ def main(check_poetry_path, copy_clipboard):
     set path of poetry project, eg.\n
     $ poetry run ppcheck ~/poetry-project
     """
-    _title = "|         Poetry pyproject.toml check for script(s)         |"
+    _title = (
+        "|"
+        + ("#" * 8)
+        + " Poetry pyproject.toml check for script(s) "
+        + ("#" * 8)
+        + "|"
+    )
     _title_len = len(_title)
     print("/" + str("=" * (_title_len - 2)) + "\\")
     print(_title.upper())
@@ -44,43 +51,42 @@ def main(check_poetry_path, copy_clipboard):
         if os.path.isfile(toml_file):
             with open(toml_file, "rb") as f:
                 pp_dict = tomli.load(f)
-            if (
-                "tool" in pp_dict
-                and "poetry" in pp_dict["tool"]
-                and "scripts" in pp_dict["tool"]["poetry"]
-            ):
-                print(_create_table(pp_dict["tool"]["poetry"]["scripts"], "scripts"))
-                questions = [
-                    inquirer.List(
-                        "script",
-                        message="Choose script to execute",
-                        choices=[
-                            "poetry run {}".format(cmd)
-                            for cmd in list(pp_dict["tool"]["poetry"]["scripts"].keys())
-                        ],
-                    ),
-                    inquirer.Text(
-                        "args", message="Run w/ argument(s)", default="--help"
-                    ),
-                ]
-                answers = inquirer.prompt(questions)
-                cmd = "{} {}".format(answers["script"], answers["args"])
-                if copy_clipboard:
-                    pyperclip.copy(cmd)
-                _exec_title = (
-                    "exec: " + cmd + (" <- copy to clipboard" if copy_clipboard else "")
-                )
-                _exec_len = len(_exec_title)
-                print("-" * (_exec_len if _exec_len > _title_len else _title_len))
-                print(_exec_title)
-                print("-" * (_exec_len if _exec_len > _title_len else _title_len))
+
+            # check info
+            if _attr_exists(pp_dict, dict, "tool", "poetry"):
+                _info = pp_dict["tool"]["poetry"]
                 print(
-                    os.popen(
-                        "pushd {} > /dev/null && ".format(os.path.dirname(toml_file))
-                        + cmd
-                        + " && popd > /dev/null"
-                    ).read()
+                    _create_table(
+                        {
+                            "name": _info["name"],
+                            "version": _info["version"],
+                            "description": _info["description"],
+                            "authors": "\n".join(_info["authors"]),
+                        },
+                        "info",
+                    )
                 )
+
+            # choice what do you want?
+            q = [
+                inquirer.Checkbox(
+                    "exec_cmds",
+                    message="Run commands first by selecting with key 'space'. press 'enter' for next.",
+                    choices=["poetry lock", "poetry install"],
+                ),
+            ]
+            tasks = inquirer.prompt(q, theme=BlueComposure())
+
+            if len(tasks["exec_cmds"]) > 0:
+                for task in tasks["exec_cmds"]:
+                    if task == "poetry lock":
+                        _chk_lock(_title_len, os.path.dirname(toml_file))
+                    elif task == "poetry install":
+                        _chk_install(_title_len, os.path.dirname(toml_file))
+
+            # check scripts with inputs
+            if _attr_exists(pp_dict, dict, "tool", "poetry", "scripts"):
+                _chk_scripts(pp_dict, copy_clipboard, _title_len, toml_file)
             else:
                 print(
                     f"No script command(s) available in {os.path.basename(toml_file)}."
@@ -90,6 +96,73 @@ def main(check_poetry_path, copy_clipboard):
 
     except Exception as e:
         print(f"WARNING: Something goes wrong or aborted. {e}")
+
+
+def _chk_lock(_title_len, exec_path):
+    _print_title("Execute 'poetry lock'", _title_len)
+    _execute_cmd(exec_path, "poetry lock")
+
+
+def _chk_install(_title_len, exec_path):
+    _print_title("Execute 'poetry install'", _title_len)
+    _execute_cmd(exec_path, "poetry install")
+
+
+def _chk_scripts(pp_dict, copy_clipboard, _title_len, toml_file):
+    print(_create_table(pp_dict["tool"]["poetry"]["scripts"], "scripts"))
+    questions = [
+        inquirer.List(
+            "script",
+            message="Choose script to execute",
+            choices=[
+                "poetry run {}".format(cmd)
+                for cmd in list(pp_dict["tool"]["poetry"]["scripts"].keys())
+            ],
+        ),
+        inquirer.Text("args", message="Run w/ argument(s)", default="--help"),
+    ]
+    answers = inquirer.prompt(questions, theme=GreenPassion())
+    cmd = "{} {}".format(answers["script"], answers["args"])
+    if copy_clipboard:
+        pyperclip.copy(cmd)
+    _exec_title = "exec: " + cmd + (" <- copy to clipboard" if copy_clipboard else "")
+    _print_title(_exec_title, _title_len)
+    _execute_cmd(os.path.dirname(toml_file), cmd)
+
+
+def _print_title(title, width):
+    _exec_len = len(title)
+    print("-" * (_exec_len if _exec_len > width else width))
+    print(title)
+    print("-" * (_exec_len if _exec_len > width else width))
+
+
+def _execute_cmd(exec_path: str, cmd: str, print_it: bool = True):
+    out = os.popen(
+        "pushd {} > /dev/null && ".format(exec_path) + cmd + " && popd > /dev/null"
+    ).read()
+    if print_it:
+        print(out)
+
+
+def _attr_exists(obj_dct, should_type, *keys):
+    keys = list(keys)
+    while keys:
+        match = keys.pop(0)
+        if isinstance(obj_dct, dict):
+            if match in obj_dct:
+                if not keys:
+                    return (
+                        (True if isinstance(obj_dct[match], should_type) else False)
+                        if should_type
+                        else True
+                    )
+                else:
+                    obj_dct = obj_dct[match]
+            else:
+                return False
+        else:
+            return False
 
 
 def _create_table(entries: dict, title: str = ""):
